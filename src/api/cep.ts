19aa91d36358db7d5994df177c0b6ad21f9d9dd0
Enzo Sakamoto <enzo.sak@hotmail.com>
2023-08-07 12:01:25 -0300

    fixed try catch

--- src/api/cep.ts
+++ src/api/cep.ts
@@ -1,11 +1,6 @@
 import axios from 'axios'
 
 export async function getDataByCep(cep: string) {
-  try {
-    const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`)
-    return response.data
-  } catch (error) {
-    console.error(error)
-    return Response.error()
-  }
+  const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`)
+  return response.data
 }
